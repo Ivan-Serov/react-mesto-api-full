6f71b058a42a9fb3e643ebba976281a81860201a
Ivan Serov <van9i.serov1@gmail.com>
2022-08-29 20:04:56 +0300

    1/4

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -80,7 +80,7 @@ class Api {
 }
 
 export const api = new Api({
-  url:'https://api.mesto.IvanSerov.nomoredomains.sbs',
+  url:'http://api.mesto.IvanSerov.nomoredomains.sbs',
   headers: {
     'Content-Type': 'application/json',
   },});
